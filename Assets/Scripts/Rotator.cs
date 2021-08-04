106ca4dca218def263f8dada460c46e9446977d7
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-04 16:21:37 +0300

    [Script] coinRotate

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public AudioClip coinSound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +15,5 @@ public class Rotator : MonoBehaviour
     void Update()
     {
         transform.Rotate(45 * Time.deltaTime, 0, 0);
-    }
-
-    /// <summary>
-    /// OnTriggerEnter is called when the Collider other enters the trigger.
-    /// </summary>
-    /// <param name="other">The other Collider involved in this collision.</param>
-    void OnTriggerEnter(Collider other)
-    {
-        AudioSource.PlayClipAtPoint(coinSound, transform.position);
     }
 }
