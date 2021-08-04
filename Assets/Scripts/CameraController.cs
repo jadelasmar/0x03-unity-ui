9069f8f6b94a98db8b25cdb23eb1669200abfec9
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-04 11:11:36 +0300

    [Script] Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
