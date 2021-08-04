8ae3f31a17b428d30c117704838eab0e58fb321f
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-04 12:01:20 +0300

    [Script] Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position-player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset;
     }
 }
