9485b04a6863f642901807869465e7e5f7f33330
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-10 19:51:11 +0300

    play/quit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+    public void QuitMaze()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
 }
